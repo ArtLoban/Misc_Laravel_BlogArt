f231ba8378d9de3a9d7692498e90c4d0defee7d1
Artem Loban <tymanrock@gmail.com>
2018-07-12 13:10:12 +0300

    In progress 03

--- app/Post.php
+++ app/Post.php
@@ -4,10 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     use Sluggable;
+
+    const IS_DRAFT = 0;
+    const IS_PUBLIC = 1;
+
+    protected $fillable = ['title', 'content'];
 
     public function category(){
 
@@ -41,5 +47,107 @@ class Post extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    public static function add($fields){
+
+        $post = new static; //self
+        $post->fill($fields);
+        $post->user_id = 1;
+        $post->save();
+
+        return $post;
+    }
+
+    public function edit($fields){
+
+        $this->fill($fields);
+        $this->save();
+    }
+
+    public function remove(){
+
+        Storage::delete('uploads/' . $this->image);
+        $this->delete();
+    }
+
+    public function uploadImage($image){
+
+        if( $image == null){ return;}
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = str_random(10). '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getImage(){
+
+        if( $this->image == null ){
+
+            return '/img/no-image.png';
+        }
+
+        return '/uploads/' . $this->image;
+    }
+
+    public function setCategorey($id){
+
+        if($id == null) {return;}
+
+        $this->category_id = $id;
+        $this->save();
+    }
+
+    public function setTeg($ids){
+
+        if($ids == null) {return;}
+
+        $this->tags()->sync($ids);
+    }
+
+    public function setDraft(){
+
+        $this->status = Post::IS_DRAFT; // or just 0
+        $this->save();
+    }
+
+    public function setPublic(){
+
+        $this->status = Post::IS_PUBLIC; // or just 1
+        $this->save();
+    }
+
+    public function toggleStatus($value){
+
+        if($value == null){
+
+            return $this->setDraft();
+        }
+
+        return $this->setPublic();
+    }
+
+    public function setFeatured(){
+
+        $this->is_featured = 1; // or make const
+        $this->save();
+    }
+
+    public function setStandart(){
+
+        $this->is_featured = 0; // or make const
+        $this->save();
+    }
+
+    public function toggleFeatured($value){
+
+        if($value == null){
+
+            return $this->setStandart();
+        }
+
+        return $this->setFeatured();
     }
 }
