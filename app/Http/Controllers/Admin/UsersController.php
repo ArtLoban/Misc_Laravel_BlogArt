9af0ddab5d332fa8fb23691c1512c4e0ecd29084
Artem Loban <tymanrock@gmail.com>
2018-08-12 17:26:51 +0300

    In progress 08 - User CRUD

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
 
 class UsersController extends Controller
 {
@@ -38,7 +39,6 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required',
@@ -46,6 +46,7 @@ class UsersController extends Controller
         ]);
 
         $user = User::add($request->all());
+        $user->generatePassword($request->get('password'));
         $user->uploadAvatar($request->file('avatar'));
 
         return redirect()->route('users.index');
@@ -60,7 +61,10 @@ class UsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::find($id);
+
+        return view('admin.users.edit', compact('user'));
+        // compact('user') = ['user' => $user] Исп-ть что удобнее
     }
 
     /**
@@ -72,7 +76,23 @@ class UsersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users')->ignore($user->id),
+            ],
+            'avatar' => 'nullable|image'
+        ]);
+
+        $user->edit($request->all());
+        $user->generatePassword($request->get('password'));
+        $user->uploadAvatar($request->file('avatar'));
+
+        return redirect()->route('users.index');
     }
 
     /**
@@ -83,6 +103,8 @@ class UsersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        User::find($id)->remove();
+
+        return redirect()->route('users.index');
     }
 }
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -7,7 +7,7 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                Добавить тег
+                Добавить пользователя
                 <small>приятные слова..</small>
             </h1>
         </section>
@@ -18,32 +18,50 @@
             <!-- Default box -->
             <div class="box">
 
-                {!! Form::open(['route' => ['tags.update', $tag->id],
-                                'method' => 'put']) !!}
+                {!! Form::open([
+                    'route' => ['users.update', $user->id],
+                    'method' => 'put',
+                    'files' => true
+                ]) !!}
 
                 <div class="box-header with-border">
-                    <h3 class="box-title">Меняем тег</h3>
+                    <h3 class="box-title">Добавляем пользователя</h3>
                 </div>
                 <div class="box-body">
                     <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Название</label>
-                            <input type="text" class="form-control" name="title" id="exampleInputEmail1"
-                                   placeholder="" value="{{ $tag->title }}">
-                        </div>
 
                         @include('admin.errors')
 
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Имя</label>
+                            <input type="text" class="form-control" name="name" id="exampleInputEmail1" placeholder="" value="{{ $user->name }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">E-mail</label>
+                            <input type="text" class="form-control" name="email" id="exampleInputEmail1" placeholder="" value="{{ $user->email }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Пароль</label>
+                            <input type="password" class="form-control" name="password" id="exampleInputEmail1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <img src="{{ $user->getAvatar() }}" alt="" width="200" class="img-responsive">
+                            <label for="exampleInputFile">Аватар</label>
+                            <input type="file" name="avatar" id="exampleInputFile">
+
+                            <p class="help-block">Какое-нибудь уведомление о форматах..</p>
+                        </div>
                     </div>
                 </div>
                 <!-- /.box-body -->
                 <div class="box-footer">
-                    <a href="{{ route('tags.index')}}" class="btn btn-default">Назад</a>
+                    <a href="{{ route('users.index')}}" class="btn btn-default">Назад</a>
                     <button class="btn btn-warning pull-right">Изменить</button>
                 </div>
                 <!-- /.box-footer-->
 
                 {!! Form::close() !!}
+
             </div>
             <!-- /.box -->
 
